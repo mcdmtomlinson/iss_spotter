aba32c6fbf4b81a31fdcf64d8681cf89c3e7da0c
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-11 23:04:48 -0600

    add fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +9,14 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+
+  fetchCoordsByIP(ip, (error, coordinates) => {
+    if (error) {
+      console.log("It didn't work!" , error);
+      return;
+    }
+
+    console.log('It worked! Returned coordinates:' , coordinates);
+  });
+
 });
