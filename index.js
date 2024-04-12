c7fbcd948afe9b5517b8c642e5605a6ec64e3486
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-11 23:17:51 -0600

    ISS Spotter III

--- index.js
+++ index.js
@@ -7,16 +7,29 @@ fetchMyIP((error, ip) => {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
-
   fetchCoordsByIP(ip, (error, coordinates) => {
     if (error) {
       console.log("It didn't work!" , error);
       return;
     }
-
+  
     console.log('It worked! Returned coordinates:' , coordinates);
   });
 
+
+
 });
+// DEPRECATED
+// const { fetchISSFlyOverTimes } = require('./iss');
+
+// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// });
