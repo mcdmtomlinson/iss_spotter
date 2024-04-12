6a2fb00ac7eb32ae992c5c2ac3f4372a016aed25
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-11 23:17:18 -0600

    ISS Spotter III

--- iss.js
+++ iss.js
@@ -25,25 +25,44 @@ const fetchMyIP = function(callback) {
   callback(null, ip);
 });
 };
-
+const fetchCoordsByIP = function(ip, callback) {
   request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
 
-    if (error) { // print error if not null
-      callback(`error: ${error}`, null); // error/null for desc
-      return;
-    }
+  if (error) { // print error if not null
+    callback(`error: ${error}`, null); // error/null for desc
+    return;
+  }
+  if (response.statusCode !== 200) { // print status code if not successful
+    const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
+  const { latitude, longitude } = JSON.parse(body);
+  callback(null, { latitude, longitude });
+});
 
-    if (response.statusCode !== 200) { // print status code if not successful
-      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
-      callback(Error(msg), null);
-      return;
-    }
-
-    const { latitude, longitude } = JSON.parse(body);
-
-    callback(null, { latitude, longitude });
-  });
-
+};
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+// DEPRECATED
+// const fetchISSFlyOverTimes = function(coords, callback) {
+//   const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+//   request(url, (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
+
+//     if (response.statusCode !== 200) {
+//       callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+//       return;
+//     }
+
+//     const passes = JSON.parse(body).response;
+//     callback(null, passes);
+//   });
+// };
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
